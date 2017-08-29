18b15dac8405756d84811a5b7fc687691747a7c0
lmutton <lornamutton@gmail.com>
2017-08-29 18:00:55 +0200

    added basic calculator functionality

--- calc/js/index.js
+++ calc/js/index.js
@@ -1,1 +1,24 @@
 //need to add funcitonality
+
+
+$(document).ready(function() {
+	var history = $("#history");
+	var result = $("#result");
+	var equals = $("#equals");
+	var clear = $("#clear");
+
+	$(".key").click(function() {
+		history.val(history.val() + $(this).attr("value"));
+	});
+	
+	$(equals).click(function() {
+		result.val(eval(history.val()));
+	});
+	
+	$(clear).click(function() {
+		history.val('')
+		result.val('0');
+	});
+	
+	
+});
