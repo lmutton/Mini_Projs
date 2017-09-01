0bfe97ec851f666897e2bda872927aba116e7146
lmutton <lornamutton@gmail.com>
2017-09-01 13:36:35 +0200

    fixed calc js layout

--- calc/README.txt
+++ calc/README.txt
@@ -1,4 +1,4 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/lmutton/pen/GvYrLX.
+A Pen created at CodePen.io. You can find this one at https://codepen.io/lmutton/pen/KvJaOG.
 
  IN PROGRESS!!!
 
--- calc/js/index.js
+++ calc/js/index.js
@@ -2,56 +2,46 @@
 
 //basic functionality complete
 $(document).ready(function() {
-	//set variables
-	var temp = [];
-	var equalsTemp = [];
-	var equalsFinal = [];
-	var history = $("#history");
-	var result = $("#result");
-	var equals = $("#equals");
-	var clear = $("#clear");
 
-	//value of key to add to history on each click
+	var currentInput = "";
+	var operators = ["x", "-", "/", "*"];
+
+	function pushToHistory() {
+		if (currentInput.length > 20) {
+			$(".history").text("Error");
+		} else {
+			$(".history").text(currentInput);
+			console.log(currentInput);
+		}
+	}
+	
+	function checkForSum(){
+		
+	}
+	
 	$(".key").click(function() {
-		history.val(history.val() + $(this).attr("value"));
-		temp.push(history.val());
-		if (temp.length >= 22) {
-			history.val("Error");
-		}
+		currentInput += $(this).attr("value");
+		pushToHistory();
 	});
-
-	// to find the total of history and put it as result
-	$(equals).click(function() {
-		//push sum to a temporary array
-		equalsTemp.push(eval(history.val()));
-		//make the tempoary array into a string
-		var numToStr = equalsTemp.toString();
-		//push each number into a new array
-		for (var i = 0; i < numToStr.length; i += 1) {
-			equalsFinal.push(+numToStr.charAt(i));
-		}
-		if (equalsFinal.length > 15) {
-			result.val("Error");
-		} 		
-		else {
-			result.val(eval(history.val()));
-		}
-		
-		
+	
+	$(".sum").click(function() {
+		currentInput += $(this).attr("value");
+		pushToHistory();
 	});
-
-	//clear both history and result, and put back to 0
-	$(clear).click(function() {
-		history.val("");
-		result.val("0");
-		temp = [];
-		equalsTemp = [];
-		equalsFinal = [];
+	
+	$(".decimal").click(function() {
+		currentInput += $(this).attr("value");
+		pushToHistory();
 	});
-
-	//only allow 1 sum/decimal symbol
-	//only allow ' - ' as first possible sum symbol
-	//fix decimal to allow zero before it
-
-	//bonus fix - change x to * and ÷ to /
+	
+	$(".equals").click(function() {
+		$(".result").text(eval(currentInput));	
+	});
+	
+	$(".clear").click(function() {
+		$(".history").text('0');
+		$(".result").text('0');
+		currentInput = '';
+	});
+	
 });
