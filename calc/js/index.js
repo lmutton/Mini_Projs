5536723762f4b5be8e2e235332f0599f083517f7
lmutton <lornamutton@gmail.com>
2017-09-01 15:23:34 +0200

    now unable to hit multiple sum symbols

--- calc/js/index.js
+++ calc/js/index.js
@@ -2,46 +2,71 @@
 
 //basic functionality complete
 $(document).ready(function() {
-
 	var currentInput = "";
-	var operators = ["x", "-", "/", "*"];
+	var operators = ["+", "-", "x", "÷"];
+	var sum = false;
+	var decimal = false;
 
 	function pushToHistory() {
 		if (currentInput.length > 20) {
-			$(".history").text("Error");
+			$(".history").text("too long :(");
 		} else {
 			$(".history").text(currentInput);
-			console.log(currentInput);
 		}
 	}
-	
-	function checkForSum(){
-		
-	}
-	
+
 	$(".key").click(function() {
 		currentInput += $(this).attr("value");
 		pushToHistory();
+		decimal = false;
+		sum = false;
 	});
-	
+
 	$(".sum").click(function() {
-		currentInput += $(this).attr("value");
-		pushToHistory();
+		if (!sum) {
+			currentInput += $(this).attr("value");
+			pushToHistory();
+		}
+		sum = true;
+		decimal = false;
 	});
-	
+
 	$(".decimal").click(function() {
-		currentInput += $(this).attr("value");
-		pushToHistory();
+		if (!decimal) {
+			currentInput += $(this).attr("value");
+			pushToHistory();
+		}
+		decimal = true;
+		sum = false;
 	});
-	
+
 	$(".equals").click(function() {
-		$(".result").text(eval(currentInput));	
+		var toEquate = currentInput;
+		var lastChar = toEquate[toEquate.length - 1];		
+		//replace x and ÷ with * and / to get results
+		toEquate = toEquate.replace(/x/g, "*").replace(/÷/g, "/");
+		//if the last digit is a sum or decimal, replace with nothing
+		if (operators.indexOf(lastChar) > -1 || lastChar == ".") {
+			toEquate = toEquate.replace(/.$/, "");
+		}
+
+		//change history and result text
+		var toHistory = toEquate.replace('*', 'x').replace('/', '÷');
+		$(".history").text(toHistory);		
+		if(toEquate.length > 15){
+			$(".result").text("too long :(")
+		}else{
+		$(".result").text(eval(toEquate));
+			}
+		decimal = false;
+		sum = false;
 	});
-	
+
 	$(".clear").click(function() {
-		$(".history").text('0');
-		$(".result").text('0');
-		currentInput = '';
+		$(".history").text("0");
+		$(".result").text("0");
+		currentInput = "";
+		decimal = false;
+		sum = false;
 	});
-	
 });
