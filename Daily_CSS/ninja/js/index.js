9f6ece2532c563bd12b86aa6e0be462f9a596a38
lmutton <lornamutton@gmail.com>
2017-09-13 13:58:25 +0200

    updates ninja

--- Daily_CSS/ninja/js/index.js
+++ Daily_CSS/ninja/js/index.js
@@ -1,6 +1,14 @@
-$('.container').hover(function(){
-	$('.ninja').fadeOut('fast')
-},
-function(){
-	$('.ninja').fadeIn()
-});
+var blink = function (blink){
+	var eyeLeft = document.getElementById('eye-l');
+	var eyeRight = document.getElementById('eye-r');
+	
+	eyeLeft.className = 'closed-l';
+	eyeRight.className = 'closed-r';
+	
+	setTimeout(function(){
+		eyeLeft.className = 'eye-l';
+		eyeRight.className = 'eye-r';		
+	}, 200);	
+}
+
+window.onload = setInterval(blink, 2000);
